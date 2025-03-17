7d6edc1730fd90a2ef78f834301f637cb609e7c5
AndroidMaster <ya-mihim@yandex.ru>
2025-03-17 22:53:09 +0300

    nav_host_switched

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,9 @@ android {
 
 dependencies {
 
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("androidx.fragment:fragment-ktx:1.5.6")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
