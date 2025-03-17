c305bafc61bc7409d76ea0e5a3de4b6c4378f0f8
AndroidMaster <ya-mihim@yandex.ru>
2025-03-17 19:43:54 +0300

    xml_ready

---
+++ app/src/main/java/com/example/way/HostActivity.kt
@@ -0,0 +1,16 @@
+package com.example.way
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.way.databinding.ActivityHostBinding
+
+class HostActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHostBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
